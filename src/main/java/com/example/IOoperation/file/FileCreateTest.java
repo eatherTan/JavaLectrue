b718d2ff4c0f3ea52643d6fdebc538dd02dea139
xiaolian.tan <xiaolian.tan@ly.com>
2020-03-26 21:12:31 +0800

    java io

---
+++ src/main/java/com/example/IOoperation/file/FileCreateTest.java
@@ -0,0 +1,10 @@
+package com.example.IOoperation.file;
+
+import java.io.File;
+
+public class FileCreateTest {
+    public static void main(String[] args) {
+        File file = new File("D:\\ProjectCode\\test.txt");
+
+    }
+}
