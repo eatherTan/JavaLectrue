919dd9718e2de615c1eaea85599daa7e9eefd30e
xiaolian.tan <xiaolian.tan@ly.com>
2020-05-07 16:10:51 +0800

    leetcode算法：Z字String

---
+++ src/main/java/com/example/leetcode/IsPalindrome.java
@@ -0,0 +1,19 @@
+package com.example.leetcode;
+
+public class IsPalindrome {
+    /**
+     * 验证是否是回文串
+     * @param s
+     * @return
+     */
+//    public boolean isPalindrome(String s) {
+//
+//        if (s.length() == 0){
+//            return true;
+//        }
+//        int left = 0;
+//        int right = s.length() - 1;
+//
+//
+//    }
+}
