5ac8325632eb05e47f311587bda6f71c04ab2a94
xiaolian.tan <eather_tan@163.com>
2020-09-09 19:18:02 +0800

    单链表添加节点

--- src/main/java/com/example/lineartable/MySingleLinked.java
+++ src/main/java/com/example/lineartable/MySingleLinked.java
@@ -4,22 +4,67 @@ package com.example.lineartable;
  线性表
  */
 public class MySingleLinked {
+    public static void main(String[] args) {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        HeroNode hero1 = new HeroNode(1,"宋江","及时雨");
+        HeroNode hero2 = new HeroNode(2,"宋江11","及时雨234");
+        singleLinkedList.addNode(hero1);
+        singleLinkedList.addNode(hero2);
+        singleLinkedList.showList();
+    }
+}
 
 //定义 SingleLinkedList 管理英雄
 class SingleLinkedList{
     private HeroNode head = new HeroNode(0,"","");
-    //添加节点
-    public void addNode(){
 
+    //添加节点，第一种方式，添加到链表尾部
+    public void addNode(HeroNode newNode){
+        if (head.next == null){
+            head.next = newNode;
+        }else {
+            HeroNode temp = head.next;
+            while (temp != null) {
+                if (temp.next == null) {
+                    temp.next = newNode;
+                    break;
+                }
+                temp = temp.next; //后移一个位置
+            }
+        }
+    }
+
+    //第二种添加节点的方式：按照no的大小来排序
+    public void addByOrder(HeroNode newNode){
+        if (head.next == null){
+            head.next = newNode;
+        }else {
+            HeroNode temp = head.next;
+            while (temp != null){
+                if (temp.no == newNode.no){
+
+                }
+                if (temp.no < newNode.no && temp.no > newNode.no){
+                    newNode.next = temp.next;
+                    temp.next = newNode;
+                }
+            }
+        }
+    }
+    //展示链表的信息
+    public void showList(){
+        HeroNode temp = head;
+        while (temp.next != null){
+            System.out.println(temp.next.no + " " + temp.next.name + " " + temp.next.nickname);
+            temp = temp.next;
+        }
     }
 }
-
-
 class HeroNode {
-    private int no;
-    private String name;
-    private String nickname;
-    private HeroNode next;
+    public int no;
+    public String name;
+    public String nickname;
+    public HeroNode next;
 
     public HeroNode(int no, String name, String nickname) {
         this.no = no;
@@ -36,6 +81,4 @@ class HeroNode {
                 ", next=" + next +
                 '}';
     }
-}
-
 }
