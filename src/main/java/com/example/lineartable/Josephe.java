07e0cad3d8a14215e1b939828d3cdebf18136713
xiaolian.tan <eather_tan@163.com>
2020-11-28 11:38:08 +0800

    插入排序 和 约瑟夫环

---
+++ src/main/java/com/example/lineartable/Josephe.java
@@ -0,0 +1,20 @@
+package com.example.lineartable;
+
+/**
+ * 约瑟夫问题
+ */
+public class Josephe {
+    public static void main(String[] args) {
+
+    }
+}
+
+//常见一个Boy类，来表示一个节点
+class Boy{
+    public int no;
+    public Boy next;
+
+    public Boy(int no) {
+        this.no = no;
+    }
+}
