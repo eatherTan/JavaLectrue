bd1f5a13b1669e10a8044a67742aabf5d275466b
xiaolian.tan <eather_tan@163.com>
2020-12-28 10:18:36 +0800

    爬楼梯

---
+++ src/main/java/com/example/leetcode/ClimbStairs70.java
@@ -0,0 +1,12 @@
+package com.example.leetcode;
+
+public class ClimbStairs70 {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//
+//    public static int climbStairs(int n) {
+//
+//    }
+}
---
+++ src/main/java/com/example/leetcode/LengthOfLastWord58.java
@@ -0,0 +1,42 @@
+package com.example.leetcode;
+
+public class LengthOfLastWord58 {
+    public static void main(String[] args) {
+//        String s = "Hello World! ";
+        String s = "t";
+        System.out.println(lengthOfLastWord(s));
+        System.out.println(lengthOfLastWord2(s));
+    }
+
+    /**
+     * 使用split()
+     * @param s
+     * @return
+     */
+    public static int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        if (str.length == 0){
+            return 0;
+        }
+        return str[str.length-1].length();
+    }
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLastWord2(String s) {
+        int count = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) == ' ' && count == 0 ){
+                continue;
+            }else if (s.charAt(i) != ' '){
+                count++;
+            }else if (count != 0 && s.charAt(i) == ' '){
+                break;
+            }
+        }
+        return count;
+    }
+}
