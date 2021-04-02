6629e758717833f8891829dd44e4af2b0c64cefa
xiaolian.tan <eather_tan@163.com>
2021-04-02 19:09:48 +0800

    使用 lamda语法 结合 排序

--- src/main/java/com/example/lamda/StreamLamdaTest.java
+++ src/main/java/com/example/lamda/StreamLamdaTest.java
@@ -6,10 +6,18 @@ import java.util.stream.Collectors;
 
 public class StreamLamdaTest {
     public static void main(String[] args) {
+
+        //List的lamda操作
         List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
         List<String> list1 = list.stream().filter(s -> s.equals("3")).collect(Collectors.toList());
         System.out.println(list1.get(0));
+
+        List<String> strings = new ArrayList<>();
+        strings.add("ta");
+        strings.add(" dkj");
+        String update = strings.stream().reduce("",String::concat);
+        System.out.println(update);
     }
 }
