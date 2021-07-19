db3c90cb2931a3a214436e7708f4fd4aa3245860
xiaolian.tan <eather_tan@163.com>
2021-07-19 13:31:34 +0800

    递归反转链表

--- src/main/java/com/example/leetcode/L206ReverseListNode.java
+++ src/main/java/com/example/leetcode/L206ReverseListNode.java
@@ -1,7 +1,18 @@
 package com.example.leetcode;
 
 public class L206ReverseListNode {
-
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode node = reverseList(node1);
+        ListNode newNode = reverseListNodeByRecursion(node);
+        System.out.println(1);
+    }
     /**
      * 使用头插法反转链表
      */
@@ -17,5 +28,21 @@ public class L206ReverseListNode {
             pre = temp;
         }
         return cur;
+    }
+
+    /**
+     * 使用递归的方式反转链表
+     */
+    public static ListNode reverseListNodeByRecursion(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+        //仔细思考：为什么反转后的链表要把头节点存起来
+        //如果不用ListNode newHead = reverseListNodeByRecursion(head.next);  把头节点存起来，就会得到最后那个节点，但是因为进行了head.next = null;
+        //所以在拿到最后那个节点时，也找不到其他节点了。head.next = null
+        ListNode newHead = reverseListNodeByRecursion(head.next);
+        head.next.next = head;
+        head.next = null;//为了不形成环形链表
+        return newHead;
     }
 }
