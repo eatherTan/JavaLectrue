f8f9a0ce019c82ec315172f915521323055bb5fe
eatherTan <eather_tan@163.com>
2021-08-01 13:10:07 +0800

    找到目标数target

--- src/main/java/com/example/leetcode/L001TwoSum.java
+++ src/main/java/com/example/leetcode/L001TwoSum.java
@@ -19,7 +19,7 @@ public class L001TwoSum {
     public static void main(String[] args) {
         int[] nums = {3,3};
         int target = 6;
-        int[] result = twoSum(nums,target);
+        int[] result = twoSumByBinarySearch(nums,target);//twoSum(nums,target);
         System.out.println(result[0] + "" + result[1]);
     }
 
@@ -38,5 +38,42 @@ public class L001TwoSum {
         result[0]=-1;
         result[1]=-1;
         return result;
+    }
+
+    /**
+     * 题目变换：如果给定的数组是有序的，现在有一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * 解题思路：可以使用 二分法查找存不存在 x-num[i] ，不存在的话，就i自增，继续查找；找到的话，就返回这两个数的位置
+     * 相对于暴雨解法，使用二分法查找元素上更快。时间复杂度是nlogN
+     *
+     */
+    public static int[] twoSumByBinarySearch(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i<nums.length; i++){
+            int j = findNumNByBinarySearch(nums,target-nums[i],i+1,nums.length-1);
+            if (j>0){
+                result[0]=i;
+                result[1]=j;
+                return result;
+            }
+
+        }
+        result[0]=-1;
+        result[1]=-1;
+        return result;
+    }
+
+    public static int findNumNByBinarySearch(int[] nums, int target,int left, int right){
+        int mid = 0;
+        while (left<=right){
+            mid = (left+right)/2;
+            if (target < nums[mid]){
+                right=mid-1;
+            }else if (target > nums[mid]){
+                left=mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
--- src/main/java/com/example/leetcode/L628ThreeNumsOfMaximumProduct.java
+++ src/main/java/com/example/leetcode/L628ThreeNumsOfMaximumProduct.java
@@ -4,4 +4,13 @@ package com.example.leetcode;
  * 三个数的最大乘积
  */
 public class L628ThreeNumsOfMaximumProduct {
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4,5,5,6,7,7};
+        System.out.println(getMaxProduct(nums));
+
+    }
+    public static int getMaxProduct(int[] num){
+        int n = num.length;
+        return Integer.max(num[0]*num[1]*num[2],num[n-1]*num[n-2]*num[n-3]);
+    }
 }
